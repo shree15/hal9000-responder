6404327514acaaf9708c2ed8cd1ac4b08d44211f
PraiseBeToScience <pbts.reddit@gmail.com>
2017-02-05 15:57:34 -0500

    cleaned up versioning

--- hal9000/plugins/__init__.py
+++ hal9000/plugins/__init__.py
@@ -2,10 +2,12 @@
 # pylint: disable=c0103
 import praw
 
-from slackbot.settings import REDDIT
+from slackbot_settings import REDDIT
+from hal9000 import __version__
 
+USER_AGENT = "linux:{}:hal9000-responder by /u/praisebetoscience"
 reddit = praw.Reddit(client_id=REDDIT['CLIENT_ID'],
                      client_secret=REDDIT['CLIENT_SECRET'],
-                     user_agent=REDDIT['USER_AGENT'],
+                     user_agent=USER_AGENT.format(__version__),
                      username=REDDIT['USER'],
                      password=REDDIT['PASSWORD'])
